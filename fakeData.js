bcc67b6c4fe708e0830b4ffa2cf9b11eee6a1825
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-06-11 21:26:54 -0300

    refact: now this function is creating new users.

--- fakeData.js
+++ fakeData.js
@@ -1,4 +1,3 @@
-
 const fakeData  =  [
     {
         id: 1,
--- teste2.js
+++ teste2.js
@@ -1,17 +1,20 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
+const fs = require("fs/promises")
 
-module.exports = function(req, res){
-  
-    var name =  req.body.name;
-    var jov =  req.body.job;
+async function insertNewUser(req, res) {
+    const { name, job } = req.body;
+    const lastId = data[data.length - 1].id;
     
-    var newUser = {
-        name: name,
-        job: job,
+    const newUser = {
+        id: lastId + 1,
+        name,
+        job,
     }
 
-    data.push(newUser)
-    
-    res.send(newUser);
-
+    data.push(newUser);
+    return res.status(201).json(newUser);
 };
+
+module.exports = {
+    insertNewUser,
+}
