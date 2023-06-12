baa4ce4eec2078546dc937736bd5b4eb894a6ac6
Fernando Dornelles Rocha <fe.dornellesrocha@outlook.com>
2023-06-11 23:54:45 -0300

    chore: add more data to DB

--- src/fakeData.js
+++ src/fakeData.js
@@ -2,8 +2,26 @@ const fakeData  =  [
     {
         id: 1,
         name: "João Oliveira",
+        password: "123",
         job: "Desenvolvedor",
         counter: 0,
+        role: "admin",
+    },
+    {
+        id: 2,
+        name: "Fernando",
+        password: "123",
+        job: "Desenvolvedor",
+        counter: 0,
+        role: "user",
+    },
+    {
+        id: 3,
+        name: "Diógenes",
+        password: "123",
+        job: "Filósofo",
+        counter: 0,
+        role: "user",
     }
 ]
 
